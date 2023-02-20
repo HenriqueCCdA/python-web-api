58bce6d212537bcd491f0f7fe4b86ed661fa043d
Henrique Andrade <henrique.ccda@gmail.com>
2023-02-20 15:19:42 -0300

    d01.05

--- exemplos/day1/http_client.py
+++ exemplos/day1/http_client.py
@@ -2,10 +2,10 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(("example.com", 80))
+client.connect(("localhost", 9000))
 
 # request
-cmd = "GET http://example.com/index.html HTTP/1.0\r\n\r\n".encode()
+cmd = "GET http://localhost/index.html HTTP/1.0\r\n\r\n".encode()
 
 client.send(cmd)
 
@@ -13,6 +13,6 @@ while True:
     data = client.recv(512)
     if len(data) < 1:
         break
-    print(data.decode(), end="")
+    print('1', data.decode())
 
 client.close()
