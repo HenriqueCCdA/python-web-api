131b2b1f474b13d21a561364c9e5cd9b0ccccc92
Henrique Andrade <henrique.ccda@gmail.com>
2023-02-26 17:44:45 -0300

    d01.11

--- exemplos/day1/blog_jinja/wsgi_V1.py
+++ exemplos/day1/blog_jinja/wsgi_V1.py
@@ -1,10 +1,10 @@
 import cgi
+import json
 from database import conn
 
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader("templates/"))
-
 
 
 def get_posts_from_database(post_id=None):
@@ -31,7 +31,7 @@ def add_new_post(post):
             INSERT INTO post (title, content, author)
             VALUES (:title, :content, :author)
         """,
-        post
+        post,
     )
     conn.commit()
 
@@ -39,17 +39,21 @@ def add_new_post(post):
 def application(environ, start_response):
     body = b"Content Not Found"
     status = "404 Not Found"
+    content_type = "text/html"
 
     path = environ["PATH_INFO"]
     method = environ["REQUEST_METHOD"]
 
     if path == "/" and method == "GET":
         posts = get_posts_from_database()
-        body = render_template(
-            "list.template.html",
-            post_list=posts
-        )
+        body = render_template("list.template.html", post_list=posts)
         status = "200 OK"
+
+    elif path == "/api" and method == "GET":
+        posts = get_posts_from_database()
+        status = "200 OK"
+        body = json.dumps(posts).encode("utf-8")  # serialização
+        content_type = "application/json"
 
     elif path == "/new" and method == "GET":
         body = render_template("form.template.html")
@@ -57,11 +61,9 @@ def application(environ, start_response):
 
     elif path == "/new" and method == "POST":
         form = cgi.FieldStorage(
-            fp=environ["wsgi.input"],
-            environ=environ,
-            keep_blank_values=1
+            fp=environ["wsgi.input"], environ=environ, keep_blank_values=1
         )
-        post = { item.name: item.value for item in form.list}
+        post = {item.name: item.value for item in form.list}
         add_new_post(post)
         body = b"New post created with success!"
         status = "201 Created"
@@ -69,11 +71,9 @@ def application(environ, start_response):
     elif path.split("/")[-1].isdigit() and method == "GET":
         post_id = path.split("/")[-1]
         body = render_template(
-            "post.template.html",
-            post=get_posts_from_database(post_id=post_id)[0]
+            "post.template.html", post=get_posts_from_database(post_id=post_id)[0]
         )
 
-
-    headers = [("Content-type", "text/html")]
+    headers = [("Content-type", content_type)]
     start_response(status, headers)
     return [body]
