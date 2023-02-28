abe6b44b96af9a6cd37bc8cf0673d5985495a056
Henrique Andrade <henrique.ccda@gmail.com>
2023-02-28 17:38:57 -0300

    d02.03

--- exemplos/day2/flask/app.py
+++ exemplos/day2/flask/app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
+app.config["APP_NAME"] = "Meu Blog"
+
+
+@app.errorhandler(404)
+def not_found_page(error):
+    return f"Not Found on {app.config['APP_NAME']}"
+
 
 @app.route("/")
-def hello():
-    1 /  0
-    return "<strong>Hello  World</strong>"
+def index():
+    content_url = url_for("read_content", title="Novidade de 2022")
+    return (
+        f"<h1>{app.config['APP_NAME']}</h1>"
+        f"<a href='{content_url}'>Novidades de 2022</a>"
+        "<hr>"
+        f"{dir(request)}"
+    )
+
+# @app.route("/<title>")
+def read_content(title):
+    index_url = url_for("index")
+    return f"<h1>{title}</h1> <a href='{index_url}'>Voltar</a>"
+
+app.add_url_rule("/<title>", view_func=read_content)
